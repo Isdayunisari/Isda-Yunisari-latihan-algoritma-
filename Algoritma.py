4fdd7c3577efc75897f6a55c7b7e8b7fc135523a
Isdayunisari <isdai041@gmail.com>
2024-10-23 21:51:05 +0800

    Add files via upload

---
+++ Algoritma.py
@@ -0,0 +1,16 @@
+#pertambahan
+a = 2
+b = 1
+nilai = a + b
+print(nilai)
+#pengurangan
+nilai = a - b
+print(nilai)
+#perkalian
+c = 6
+d = 2
+nilai = c * d
+print(nilai)
+#pembagian
+nilai = c / d
+print(nilai)
