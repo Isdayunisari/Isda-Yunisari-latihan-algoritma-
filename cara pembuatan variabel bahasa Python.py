a8a778558ac241f32cf83f7a1b386f1cae74ade4
Isdayunisari <isdai041@gmail.com>
2024-10-30 11:07:11 +0800

    Add files via upload

---
+++ cara pembuatan variabel bahasa Python.py
@@ -0,0 +1,29 @@
+#pembuatan variabel python dan menampilkan hasilnya
+nama = "Isda yunisari"
+print(nama)
+umur = 18
+print(umur)
+data = True
+print(data)
+
+#mengubah nilai variabel
+nama = "Byun baekhyun"
+print(nama)
+umur = 32
+print(umur)
+data = False
+print(data)
+
+#foo (contoh fungsi sederhana)
+foo = "Hello"
+print(foo)
+foo = 14
+print(foo)
+foo = True
+print(foo)
+
+#nilai konstanta
+bulan2 = "Februari"
+print(bulan2)
+PI = 3.14
+print(PI)
