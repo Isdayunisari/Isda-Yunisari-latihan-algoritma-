e6049eb070f602b7703b2a31ef2c69e8316e6502
Isdayunisari <isdai041@gmail.com>
2024-10-30 19:12:16 +0800

    Add files via upload

---
+++ tipe data string.py
@@ -0,0 +1,17 @@
+#tipe data string
+data_string = 'Teks ini\nakan dipecah\nke dalam 3 baris'
+print("data : ", data_string)
+print("-bertipe : ", type(data_string))
+#data string menggunakan tanda kutip dua("")
+data = "Teks tersebut akan dipecah\nke dalam 2 baris"
+print("data : ", data_string)
+print("-bertipe : ", type(data_string))
+#data string menggunakan tanda kutip satu atau dua sebanyak 3 kali ( ' ' ' ) atau (" " ")
+data_string = '''teks ini merupakan output data string menggukan tanda kutip sebanyak 3 kali'''
+print(data_string)
+#operasi penyambungan tipe data string
+a = 'main'
+b = "volly ball"
+c = "di lapangan"
+d = a+b+c
+print (d)
