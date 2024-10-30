1ebb9a84a41f7394c56d1c24eb7159ca0e8dd983
Isdayunisari <isdai041@gmail.com>
2024-10-30 19:06:15 +0800

    Add files via upload

---
+++ tipe data python.py
@@ -0,0 +1,37 @@
+# tipe data integer
+umur = 18
+print(umur)
+
+#tipe data float
+tinggi = 152.4
+print(tinggi)
+
+#tipe data string
+nama = "isda yunisari"
+print"nama"
+
+#tipe data boolean
+data = True
+print(data)
+
+#tipe data complex number
+data = 3+4j
+print(data)
+
+
+#Tipe Data List
+data = ["satu","dua","tiga","satu"]
+print(data)
+ 
+#Tipe Data Tuple
+data  = ("satu","dua","tiga","satu")
+print(data)
+ 
+#Tipe Data Set
+data_set = {"satu","dua","tiga","empat"}
+print(data)
+
+ 
+#Tipe Data Dictionary
+data_dictionary = {"satu":1, "dua":2.13, "tiga":"a", "empat": True}
+print(data_dictionary)
