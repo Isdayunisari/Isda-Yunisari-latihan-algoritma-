bb4d2496d591cac76b74233f26c8daca56ac6d50
Isdayunisari <isdai041@gmail.com>
2024-10-31 23:44:02 +0800

    Add files via upload

---
+++ operator penugasan.py
@@ -0,0 +1,21 @@
+#operator penugasan
+#penjumlahan
+a = 4
+a += 3 
+print(a)  
+#pengurangan
+x = 15
+x -= 5 
+print(x)  
+#perkalian
+c = 5
+c *= 6
+print(c)  
+#pembagian
+e = 10
+e /= 2 
+print(e)  
+#modulo
+h = 10
+h %= 3  
+print(h)  
