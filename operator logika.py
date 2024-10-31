c2fe52dea15c870c0d001d97dd609e160b20b274
Isdayunisari <isdai041@gmail.com>
2024-10-31 23:35:34 +0800

    Add files via upload

---
+++ operator logika.py
@@ -0,0 +1,24 @@
+#operator logika
+#and
+a = -5
+b = 10
+
+if a > 0 or b > 0:
+    print("Salah satu dari a atau b positif.")
+else:
+    print("Keduanya tidak positif.")
+#or
+a = -5
+b = 10
+
+if a > 0 or b > 0:
+    print("Salah satu dari a atau b positif.")
+else:
+    print("Keduanya tidak positif.")
+ #not
+ is_raining = False
+
+if not is_raining:
+    print("Cuaca cerah, silakan keluar!")
+else:
+    print("Bawa payung!")
