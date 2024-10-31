c512e2b4a04e64c8a9b2139bebfde280fdb59883
Isdayunisari <isdai041@gmail.com>
2024-10-31 23:32:33 +0800

    Add files via upload

---
+++ operator perbandingan.py
@@ -0,0 +1,22 @@
+#operator perbandingan
+#==(sama dangan)
+a = 10
+b = 10
+
+if a == b:
+    print("a sama dengan b.")
+else:
+    print("a tidak sama dengan b.")
+ #tidak sama dengan !=
+a = 10
+b = 5
+
+if a != b:
+    print("a tidak sama dengan b.")
+else:
+    print("a sama dengan b.")
+  #kurang dari dan lebih dari
+a = 2
+b = 4
+print(a < b)
+print(b > a)
