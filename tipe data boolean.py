76141cd56b81a0929f9b4f55e45cab77f4f6e62b
Isdayunisari <isdai041@gmail.com>
2024-10-31 21:52:54 +0800

    Add files via upload

---
+++ tipe data boolean.py
@@ -0,0 +1,8 @@
+#tipe data booolean adalah tipe data yang memiliki dua nilai yaitu true/flase
+#tipe data boolean dengan nilai true
+data = True
+print( data)
+
+#tipe data boolean dengan nilai false
+data= False
+print(data)
