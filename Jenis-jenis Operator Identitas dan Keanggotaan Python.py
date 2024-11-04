e74e40cd5b4858592b1e87257e4cf1af766bf0fd
Isdayunisari <isdai041@gmail.com>
2024-11-04 19:47:24 +0800

    Add files via upload

---
+++ Jenis-jenis Operator Identitas dan Keanggotaan Python.py
@@ -0,0 +1,13 @@
+#Operator Identitas Python
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+print(a is b) 
+print(a is c)  
+print(a is not c)  
+#Operator Keanggotaa
+ Pythonlist_buah = ['apel', 'jeruk', 'mangga']
+print('apel' in list_buah)  
+print('pisang' in list_buah)  
+print('jeruk' not in list_buah)   
+print('kiwi' not in list_buah)  
