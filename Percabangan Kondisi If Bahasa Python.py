27a6f847415429680b5b3184f02b396da79e795b
Isdayunisari <isdai041@gmail.com>
2024-11-04 19:58:17 +0800

    Add files via upload

---
+++ Percabangan Kondisi If Bahasa Python.py
@@ -0,0 +1,13 @@
+#Percabangan Kondisi If Bahasa Python
+# Contoh sederhana
+nilai = 75
+
+if nilai >= 85:
+    print("Nilai Anda A")
+elif nilai >= 70:
+    print("Nilai Anda B")
+elif nilai >= 60:
+    print("Nilai Anda C")
+else:
+    print("Nilai Anda D")
+        
