9753b252cb129a5f26f411adf05ad097f50feb24
Isdayunisari <isdai041@gmail.com>
2024-11-04 19:46:55 +0800

    Add files via upload

---
+++ Jenis-jenis Operator Assignment Python.py
@@ -0,0 +1,29 @@
+#Operator Assignment Python
+a = 2
+b = 3
+c = a + b
+d = c + a +b
+e = (a + b)* c
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+#Operator Assignment Gabungan Python
+a = 2
+b+= 10
+print('a += 5  :',b)
+  
+c = 10
+d/= 5
+print('c /= 5  :',d)
+  
+e = 4
+f**= 2
+print('e **= 5 :',f)
+  
+g = 10
+h <<= 2
+print('g <<= 2 :',h)
+
+
